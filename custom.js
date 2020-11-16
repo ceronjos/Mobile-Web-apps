72a3d7e034baab21afca75ea3d44da44d45c528a
ceronjos <72771010+ceronjos@users.noreply.github.com>
2020-11-15 23:10:44 -0500

    Add files via upload
    assignment 2 changes baby

---
+++ custom.js
@@ -0,0 +1,18 @@
+if (Modernizr.geolocation) {
+  // supported 
+  console.log("supported")
+} else {
+  // not-supported
+  console.log("unsupported")
+} 
+console.log(Modernizr)
+
+if (Modernizr.progressbar,meter) {
+  // supported
+  console.log("supported")
+} else {
+  // not-supported
+  console.log("unsupported")
+}
+console.log(Modernizr)
+
